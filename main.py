defbb149ad963eedd23fcebe366aa17a0db7d0f4
AbstractGhoul05 <aayushb14@gmail.com>
2020-06-30 23:12:27 +0530

    Add logging and some commands

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import itertools
 
 import asyncio
 
+import logging
+
 import requests
 from urllib.parse import urlparse
 
@@ -50,6 +52,13 @@ subreddit = reddit.subreddit(
         f"{'+'.join(sr for sr in config['content']['subreddits'])}"
     )
 
+logging.basicConfig(
+    filename = 'memebot.log',
+    filemode ='w',
+    format = '%(asctime)s: %(levelname)s - %(message)s',
+    level = getattr(logging, 'DEBUG' if config['debug']['verbose'] else 'INFO')
+)
+
 async def get_posts():
     return set(
         itertools.chain.from_iterable(
@@ -72,26 +81,52 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == guild_name:
             break
-    print(f'{client.user} has connected to the following guild:\n', 
+    logging.info(f'{client.user} has connected to the following guild:\n', 
     f'{guild.name}(id: {guild.id})')
     client.loop.create_task(loop_through_memes())
 
 async def send_memes():
-    posts = await get_posts()
-    for post in posts:
-        check_published = Query()
-        is_published = bool(db.search(check_published.post_id == post.id))
-        if is_published:
-            continue
-        media_url = await get_direct_link(post)
-        meme_channel = client.get_channel(config['discord']['channel_id'])
-        await meme_channel.send(media_url)
-        await meme_channel.send(f"{post.title}\nSource: [{post.subreddit_name_prefixed}]({post.shortlink})")
-        db.insert({'post_id': post.id})
-        await asyncio.sleep(300)
+    try:
+        posts = await get_posts()
+        for post in posts:
+            logging.info(f'Posting meme: {post.id}')
+            check_published = Query()
+            is_published = bool(db.search(check_published.post_id == post.id))
+            if is_published:
+                continue
+            media_url = await get_direct_link(post)
+            meme_channel = client.get_channel(config['discord']['channel_id'])
+            await meme_channel.send(media_url)
+            await meme_channel.send(f"{post.title}\nSource: {post.shortlink} ({post.subreddit_name_prefixed})")
+            logging.info(f'Succesfully posted meme: {post.id}')
+            db.insert({'post_id': post.id})
+            await asyncio.sleep(300)
+    except Exception as e:
+        client.get_channel(config['discord']['channel_id']).send(f"I've encountered some error while posting the meme!\nKindly type !memelogs to get more info")
+        logging.error(f"Error: {e}")
 
 async def loop_through_memes():
     while True:
         await send_memes()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'm!help':
+        await message.channel.send("I'm a meme bot created by AbstractGhoul05!\nList of Available Commands:\nm!logs: fetch logs\nm!clearlogs: clear logs\nm!ping: Pong!")
+
+    if message.content == 'm!logs':
+        logging.info("Sending logs")
+        await message.channel.send(file=discord.File('memebot.log'))
+
+    if message.content == 'm!clearlogs':
+        os.remove('memebot.log')
+        logging.info('Cleared logs')
+
+    if message.content == 'm!ping':
+        message.channel.send('Pong!')
+        logging.info('Send Pong!')
 
 client.run(bot_token)
